feecb1417402e6f855f40d99614c6cfdeb8b8b3f
GOOMBANGEE <goom@goomba.org>
2025-02-10 21:16:18 +0900

    refactor: relationMode="prisma" community.service.ts에서 최근 10개 게시글 가져오는 방식을 queryRaw에서 prisma relation으로 변경

--- src/community/community.service.ts
+++ src/community/community.service.ts
@@ -7,7 +7,7 @@ import {
   CommunityException,
 } from '../common/exception/community.exception';
 import { RequestUser } from '../auth/decorator/user.decorator';
-import { Community, Post, Prisma } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class CommunityService {
@@ -29,77 +29,23 @@ export class CommunityService {
   // /community?page=number
   // return: {communityList: (Community & { postList: Post[] })[], total: community.count, page: currentPage, totalPage}
   async communityList(page: number) {
-    const limit = 20;
-    const [communities, total] = await this.prisma.$transaction([
+    const limit = 10;
+    const [communityList, total] = await this.prisma.$transaction([
       this.prisma.community.findMany({
-        skip: (page - 1) * limit,
         take: limit,
         select: {
           id: true,
           title: true,
+          Post: {
+            take: limit,
+            orderBy: {
+              id: 'desc',
+            },
+          },
         },
       }),
       this.prisma.community.count(),
     ]);
-    const communityIds = communities.map(
-      (community: Community) => community.id,
-    );
-
-    // queryRaw 각 커뮤니티별 상위 10개 게시글 조회
-    // {
-    //   id: 9,
-    //   title: 'title',
-    //   creation_time: 2025-02-02T07:02:31.816Z,
-    //   comment_count: 0,
-    //   community_id: 3
-    // }
-    const snakePostList = await this.prisma.$queryRaw<Post[]>`
-    SELECT subquery.id,
-           subquery.title,
-           subquery.creation_time,
-           subquery.comment_count,
-           subquery.community_id 
-    FROM (
-        SELECT *,
-            ROW_NUMBER() OVER (
-            PARTITION BY "community_id" 
-            ORDER BY id DESC
-        ) as row_num
-        FROM "Post"
-        WHERE "community_id" IN (${Prisma.join(communityIds)})
-    ) subquery
-    WHERE row_num <= 10`;
-
-    // snake to camel
-    // {
-    //   id: 9,
-    //   title: 'title',
-    //   creationTime: 2025-02-02T07:02:31.816Z,
-    //   commentCount: 0,
-    //   communityId: 3
-    // }
-    const postList = snakePostList.map((post) => {
-      const camelCasedPost: Record<string, any> = {};
-      Object.keys(post).forEach((key) => {
-        camelCasedPost[key.replace(/_([a-z])/g, (g) => g[1].toUpperCase())] =
-          post[key];
-      });
-      return camelCasedPost;
-    });
-
-    const postCommunity = postList.reduce(
-      (acc, post: Post) => {
-        if (!acc[post.communityId]) acc[post.communityId] = [];
-        acc[post.communityId].push(post);
-        return acc;
-      },
-      {} as Record<number, Post[]>,
-    );
-
-    const communityList = communities.map((community) => ({
-      ...community,
-      postList: postCommunity[community.id] || [],
-    }));
 
     return {
       communityList,
